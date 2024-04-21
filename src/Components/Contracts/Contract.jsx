3312eb694b978b92a6288af2070ec4b4fadb22dd
Tena Spoljar <tena.spoljar@gmail.com>
2024-04-22 00:00:22 +0200

    feat: show list of products for contract details

--- src/Components/Contracts/Contract.jsx
+++ src/Components/Contracts/Contract.jsx
@@ -1,16 +1,78 @@
-import { DateField, Show, SimpleShowLayout, TextField } from "react-admin";
+import {
+	DateField,
+	Show,
+	SimpleShowLayout,
+	TextField,
+	ArrayField,
+	Datagrid,
+	FunctionField
+} from "react-admin";
+import { makeStyles } from "@material-ui/core/styles";
 
-export const Contract = () => (
-	<Show>
-		<SimpleShowLayout>
-			<TextField source='id' />
-			<TextField source='kupac' />
-			<TextField source='broj_ugovora' />
-			<DateField source='datum_akontacije' />
-			<DateField source='rok_isporuke' />
-			<TextField source='status' />
-		</SimpleShowLayout>
-	</Show>
-);
+const useStyles = makeStyles(theme => ({
+	deliveredStyle: {
+		color: "#adadad",
+		fontWeight: "bold"
+	},
+	orderedStyle: {
+		color: "#e5b700",
+		fontWeight: "bold"
+	},
+	createdStyle: {
+		color: "#61d327",
+		fontWeight: "bold"
+	}
+}));
+
+export const Contract = () => {
+	const classes = useStyles();
+
+	const renderField = (record, source) => {
+		if (!record) return null;
+
+		let statusClass;
+
+		switch (record[source]) {
+			case "KREIRANO":
+				statusClass = classes.createdStyle;
+				break;
+			case "NARUČENO":
+				statusClass = classes.orderedStyle;
+				break;
+			case "ISPORUČENO":
+				statusClass = classes.deliveredStyle;
+				break;
+		}
+
+		return <span className={statusClass}>{record[source]}</span>;
+	};
+
+	return (
+		<>
+			<Show>
+				<SimpleShowLayout>
+					<TextField source='id' />
+					<TextField source='kupac' />
+					<TextField source='broj_ugovora' />
+					<DateField source='datum_akontacije' />
+					<DateField source='rok_isporuke' />
+					<TextField source='status' />
+					<ArrayField source='products'>
+						<Datagrid bulkActionButtons={false}>
+							<TextField source='id' />
+							<TextField source='naziv' />
+							<TextField source='dobavljač' />
+							<FunctionField
+								label='Status'
+								source='status'
+								render={(record, source) => renderField(record, source)}
+							/>
+						</Datagrid>
+					</ArrayField>
+				</SimpleShowLayout>
+			</Show>
+		</>
+	);
+};
 
 export default Contract;
